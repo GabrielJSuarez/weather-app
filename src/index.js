157741847842e0925d111fb18230eee33534c68b
Gabriel J. Suarez <ginnlaiho@gmail.com>
2021-06-19 14:47:50 -0500

    - Refactor code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import {callApi} from "./async";
 import { domManipulation } from "./DOM";
 
+domManipulation.setBackground('src/winter-summer-drawn-spring-autumn-1920x1080-38359.jpg')
+
 domManipulation.preventPageReload();
 
 callApi.renderWeather();
