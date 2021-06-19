dbc0e6953d915f24db92a81a4aa09c8894086f01
Gabriel J. Suarez <ginnlaiho@gmail.com>
2021-06-19 14:52:51 -0500

    - Fix ESlint errors

--- src/DOM.js
+++ src/DOM.js
@@ -1,45 +1,44 @@
 export const domManipulation = (() => {
+  const preventPageReload = () => {
+    const FORM = document.querySelector('#weather-form');
+    FORM.addEventListener('click', (e) => {
+      e.preventDefault();
+    });
+  };
 
-    const preventPageReload = () => {
-        const FORM = document.querySelector('#weather-form');
-        FORM.addEventListener('click', (e) => {
-            e.preventDefault();
-        })
+  const getWeatherUrl = () => {
+    let API_URL_CALL;
+    let UNIT;
+    const CITY_NAME = document.querySelector('#city-name').value;
+    const CELSIUS = document.querySelector('#celsius').checked;
+
+    if (CELSIUS) {
+      API_URL_CALL = `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=51d8626e1f1408b78641a58cf1dbb707&units=metric`;
+      UNIT = 'Celsius';
+    } else {
+      API_URL_CALL = `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=51d8626e1f1408b78641a58cf1dbb707&units=imperial`;
+      UNIT = 'Fahrenheit';
     }
 
-    const getWeatherUrl = () => {
-        let API_URL_CALL;
-        let UNIT;
-        const CITY_NAME= document.querySelector('#city-name').value;
-        const CELSIUS = document.querySelector('#celsius').checked;
+    return [API_URL_CALL, UNIT];
+  };
 
-        if (CELSIUS) {
-            API_URL_CALL = `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=51d8626e1f1408b78641a58cf1dbb707&units=metric`;
-            UNIT = 'Celsius';
-        } else {
-            API_URL_CALL = `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=51d8626e1f1408b78641a58cf1dbb707&units=imperial`;
-            UNIT = 'Fahrenheit';
-        }
+  const setBackground = (imageUrl) => {
+    const BACKGROUND = document.querySelector('#bg-image');
+    BACKGROUND.style.backgroundImage = `url('${imageUrl}')`;
+    BACKGROUND.style.backgroundSize = 'cover';
+    BACKGROUND.style.backgroundPosition = 'center';
+  };
 
-        return [API_URL_CALL, UNIT];
-    }
+  const setWeatherInfo = (cityName, mainTemp, description, minTemp, maxTemp) => {
+    const CARD = document.querySelector('#weather-info');
+    const TEMP_CARD = document.querySelector('#temp-card');
+    TEMP_CARD.classList.remove('d-none');
+    const CITY = document.querySelector('#city');
 
-    const setBackground = (imageUrl) => {
-        const BACKGROUND = document.querySelector('#bg-image');
-        BACKGROUND.style.backgroundImage = `url('${imageUrl}')`;
-        BACKGROUND.style.backgroundSize = 'cover';
-        BACKGROUND.style.backgroundPosition = 'center';
-    }
+    CITY.innerHTML = `${cityName}`;
 
-    const setWeatherInfo = (cityName, mainTemp, description, minTemp, maxTemp) => {
-        const CARD = document.querySelector('#weather-info');
-        const TEMP_CARD = document.querySelector('#temp-card');
-        TEMP_CARD.classList.remove('d-none');
-        const CITY = document.querySelector('#city');
-
-        CITY.innerHTML = `${cityName}`;
-
-        CARD.innerHTML = `
+    CARD.innerHTML = `
             <li class="list-group-item bg-primary text-white border-0 text-center">
                 <span class="fw-bold">Temperature:</span> ${mainTemp}º (${domManipulation.getWeatherUrl()[1]})
             </li>
@@ -53,9 +52,11 @@ export const domManipulation = (() => {
                 <span class="fw-bold">Max Temp:</span> ${maxTemp}º (${domManipulation.getWeatherUrl()[1]})
             </li>
         `;
-    }
+  };
 
-    return {
-        preventPageReload, getWeatherUrl, setBackground, setWeatherInfo
-    }
+  return {
+    preventPageReload, getWeatherUrl, setBackground, setWeatherInfo,
+  };
 })();
+
+export default { domManipulation };
--- src/async.js
+++ src/async.js
@@ -1,33 +1,39 @@
-import { domManipulation } from "./DOM";
+import { domManipulation } from './DOM';
 
 export const callApi = (() => {
+  const getResponse = async () => {
+    try {
+      const response = await fetch(domManipulation.getWeatherUrl()[0]);
+      const weather = await response.json();
 
-    const getResponse = async () => {
-        try {
-            const response = await fetch(domManipulation.getWeatherUrl()[0]);
-            const weather = await response.json();
+      const imageURL = `https://api.unsplash.com/photos/random?client_id=NCBaOQJIb0LqtbRq7KmDTq7TLlVSqqzHoOnBVm2OVEo&query=${weather.weather[0].description}`;
+      const imageResponse = await fetch(imageURL);
+      const image = await imageResponse.json();
 
-            const imageURL = `https://api.unsplash.com/photos/random?client_id=NCBaOQJIb0LqtbRq7KmDTq7TLlVSqqzHoOnBVm2OVEo&query=${weather.weather[0].description}`;
-            const imageResponse = await fetch(imageURL);
-            const image = await imageResponse.json();
+      domManipulation.setBackground(image.urls.full);
 
-            domManipulation.setBackground(image.urls.full);
-
-            domManipulation.setWeatherInfo(weather.name, weather.main.temp, weather.weather[0].description, weather.main.temp_min, weather.main.temp_max);
-
-        } catch (err) {
-            throw new Error(err);
-        }
+      domManipulation.setWeatherInfo(
+        weather.name,
+        weather.main.temp,
+        weather.weather[0].description,
+        weather.main.temp_min,
+        weather.main.temp_max,
+      );
+    } catch (err) {
+      throw new Error(err);
     }
+  };
 
-    const renderWeather = () => {
-        const WEATHER_BTN = document.querySelector('#weather-btn');
-        WEATHER_BTN.addEventListener('click', () => {
-            getResponse();
-        })
-    }
+  const renderWeather = () => {
+    const WEATHER_BTN = document.querySelector('#weather-btn');
+    WEATHER_BTN.addEventListener('click', () => {
+      getResponse();
+    });
+  };
 
-    return {
-        renderWeather
-    }
+  return {
+    renderWeather,
+  };
 })();
+
+export default { callApi };
